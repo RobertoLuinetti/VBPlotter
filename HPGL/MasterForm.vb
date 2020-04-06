58c188a9d329c7988d6b0d8600058561646c7af7
Roberto Luinetti <luinetti@hotmail.com>
2020-04-06 23:50:46 +0200

    20200406_2350

--- HPGL/MasterForm.vb
+++ HPGL/MasterForm.vb
@@ -1,48 +1,47 @@
 ﻿Public Class MasterForm
 
     Private Sub MasterForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        SetDisplayPhysicalArea(picGraph, 960, 540, 75, 75, 708, 400)
-        SetScale(-180, 180, -50, 50)
+        SetDisplayPhysicalArea(picGraph, 960, 540, 75, 75, 810, 400) 'Select physical display area dedicated to plotter
+        SetScale(-180, 180, -50, 50) 'Set scale in logical coordinate
 
-        Dim bm As New Bitmap(Xwide, Ywide)
-        Dim gr As Graphics = Graphics.FromImage(bm)
-        gr.Clear(Color.Black)
+        Dim bm As New Bitmap(Xwide, Ywide)            'initialize graphics step 1/3
+        Dim gr As Graphics = Graphics.FromImage(bm)   'initialize graphics step 2/3
+        gr.Clear(Color.Black)                         'initialize graphics step 3/3
 
-        Frame(gr, PenGreen)
-        Axes(gr, PenGreen, 0, 0, 5, 5)
-        Grid(gr, PenGreen, 0, 0, 10, 10)
-        GraphTitle(gr, "HP-GL Plotter", "Arbutus Slab", 24)
-        XTitle(gr, "ascisse 10°/DIV", "Verdana", 10)
-        YTitle(gr, "ordinate 10/DIV", "Verdana", 10)
-        XTickLabel(gr, 30, "Verdana", 7)
-        YTickLabel(gr, 20, "Verdana", 7)
+        Frame(gr, PenGreen)                           'Draw a frame around logic area
+        Axes(gr, PenGreen, 0, 0, 5, 5)                'Draw axes 
+        Grid(gr, PenGreen, 0, 0, 10, 10)              'Draw a grid
+        GraphTitle(gr, "VB.net Plotter by Roberto Luinetti", "Arbutus Slab", 24) 'Set Graph title
+        XTitle(gr, "ascisse 10°/DIV", "Verdana", 10)                             'Set X axis title
+        YTitle(gr, "ordinate 10/DIV", "Verdana", 10)                             'Set Y axis title
+        XTickLabel(gr, 30, 30, "Verdana", 7)                                     'Set numeric scale on X axis
+        YTickLabel(gr, 20, 10, "Verdana", 7)                                     'Set numeric scale on Y axis
 
-        Draw(gr, PenRed, 0, 0, 200, 60)
-        Draw(gr, PenRed, 0, 0, 200, -60)
-        Draw(gr, PenRed, 0, 0, -200, 60)
-        Draw(gr, PenRed, 0, 0, -200, -60)
+        Draw(gr, PenRed, 0, 0, 200, 60)     'x2,y2 are outside of plotter area, segment will be truncate to out of horizontal plotter border
+        Draw(gr, PenRed, 0, 0, 200, -60)    'x2,y2 are outside of plotter area, segment will be truncate to out of horizontal plotter border
+        Draw(gr, PenRed, 0, 0, -200, 60)    'x2,y2 are outside of plotter area, segment will be truncate to out of horizontal plotter border
+        Draw(gr, PenRed, 0, 0, -200, -60)   'x2,y2 are outside of plotter area, segment will be truncate to out of horizontal plotter border
 
-        Draw(gr, PenBlue, 0, 0, 200, 40)
-        Draw(gr, PenBlue, 0, 0, 200, -40)
-        Draw(gr, PenBlue, 0, 0, -200, 40)
-        Draw(gr, PenBlue, 0, 0, -200, -40)
+        Draw(gr, PenBlue, 0, 0, 200, 40)    'x2,y2 are outside of plotter area, segment will be truncate to out of vertical plotter border
+        Draw(gr, PenBlue, 0, 0, 200, -40)   'x2,y2 are outside of plotter area, segment will be truncate to out of vertical plotter border
+        Draw(gr, PenBlue, 0, 0, -200, 40)   'x2,y2 are outside of plotter area, segment will be truncate to out of vertical plotter border
+        Draw(gr, PenBlue, 0, 0, -200, -40)  'x2,y2 are outside of plotter area, segment will be truncate to out of vertical plotter border
 
-        Dim X, Ys, Yc, Xold, Yold As Single
+        Dim X, Ys, Yc, Ys1, Xold, Yold As Single
         Dim pi As Single = 3.14159265
         For X = Xmin To Xmax Step 1
-            Ys = 60 * Math.Sin(X * pi / 180)
+            Ys = 40 * Math.Sin(X * pi / 180)
+            Ys1 = 25 * Math.Sin(X * pi / 180 * 2)
             Yc = 60 * Math.Cos(X * pi / 180)
-            If Xmin <> X Then Draw(gr, PenYellow, Xold, Yold, X, Ys)
-            Plot(gr, Color.White, X, Yc)
-            Plot(gr, Color.Aquamarine, X, Ys - Yc)
+            If Xmin <> X Then Draw(gr, PenYellow, Xold, Yold, X, Ys)  'Plot Ys function with DRAW mode (segment between previous x,y coords to actual x,y coords)
+            Plot(gr, Color.White, X, Yc)                              'Plot Yc function with PLOT mode (dot to current x,y coords)
+            Plot(gr, Color.YellowGreen, X, Ys - (Ys1 + Yc))
+            Plot(gr, Color.White, X, Ys1)
             Xold = X
             Yold = Ys
         Next
 
-
-
-
         picGraph.Image = bm
-        Clipboard.SetDataObject(DirectCast(picGraph.Image.Clone, Bitmap), True)
+        Clipboard.SetDataObject(DirectCast(picGraph.Image.Clone, Bitmap), True) 'Copy graph to clipboard
     End Sub
 End Class
