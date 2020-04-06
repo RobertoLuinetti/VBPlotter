828adc299d943ee89842124ac495569289d0e8ea
Roberto Luinetti <luinetti@hotmail.com>
2020-04-06 23:08:28 +0200

    20200406_2308

---
+++ HPGL/MasterForm.Designer.vb
@@ -0,0 +1,52 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class MasterForm
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.picGraph = New System.Windows.Forms.PictureBox()
+        CType(Me.picGraph, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picGraph
+        '
+        Me.picGraph.Location = New System.Drawing.Point(10, 12)
+        Me.picGraph.Name = "picGraph"
+        Me.picGraph.Size = New System.Drawing.Size(960, 540)
+        Me.picGraph.TabIndex = 0
+        Me.picGraph.TabStop = False
+        '
+        'MasterForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(982, 565)
+        Me.Controls.Add(Me.picGraph)
+        Me.Name = "MasterForm"
+        Me.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Text = "Master"
+        CType(Me.picGraph, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents picGraph As PictureBox
+End Class
---
+++ HPGL/MasterForm.vb
@@ -0,0 +1,48 @@
+﻿Public Class MasterForm
+
+    Private Sub MasterForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        SetDisplayPhysicalArea(picGraph, 960, 540, 75, 75, 708, 400)
+        SetScale(-180, 180, -50, 50)
+
+        Dim bm As New Bitmap(Xwide, Ywide)
+        Dim gr As Graphics = Graphics.FromImage(bm)
+        gr.Clear(Color.Black)
+
+        Frame(gr, PenGreen)
+        Axes(gr, PenGreen, 0, 0, 5, 5)
+        Grid(gr, PenGreen, 0, 0, 10, 10)
+        GraphTitle(gr, "HP-GL Plotter", "Arbutus Slab", 24)
+        XTitle(gr, "ascisse 10°/DIV", "Verdana", 10)
+        YTitle(gr, "ordinate 10/DIV", "Verdana", 10)
+        XTickLabel(gr, 30, "Verdana", 7)
+        YTickLabel(gr, 20, "Verdana", 7)
+
+        Draw(gr, PenRed, 0, 0, 200, 60)
+        Draw(gr, PenRed, 0, 0, 200, -60)
+        Draw(gr, PenRed, 0, 0, -200, 60)
+        Draw(gr, PenRed, 0, 0, -200, -60)
+
+        Draw(gr, PenBlue, 0, 0, 200, 40)
+        Draw(gr, PenBlue, 0, 0, 200, -40)
+        Draw(gr, PenBlue, 0, 0, -200, 40)
+        Draw(gr, PenBlue, 0, 0, -200, -40)
+
+        Dim X, Ys, Yc, Xold, Yold As Single
+        Dim pi As Single = 3.14159265
+        For X = Xmin To Xmax Step 1
+            Ys = 60 * Math.Sin(X * pi / 180)
+            Yc = 60 * Math.Cos(X * pi / 180)
+            If Xmin <> X Then Draw(gr, PenYellow, Xold, Yold, X, Ys)
+            Plot(gr, Color.White, X, Yc)
+            Plot(gr, Color.Aquamarine, X, Ys - Yc)
+            Xold = X
+            Yold = Ys
+        Next
+
+
+
+
+        picGraph.Image = bm
+        Clipboard.SetDataObject(DirectCast(picGraph.Image.Clone, Bitmap), True)
+    End Sub
+End Class
